d1f62a0e2e6f598469c5773717bec14035bb034e
Wesley da Silva Ribeiro <wesleysilvaribeiro9@gmail.com>
2023-06-25 20:17:50 -0300

    Create tweet (POST /tweet) implemented

--- src/app.js
+++ src/app.js
@@ -9,10 +9,18 @@ const PORT = 5000;
 
 // Data variables
 const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const user = req.body;
     users.push(user);
+    res.send("OK");
+});
+
+app.post("/tweet", (req, res) => {
+    const tweet = req.body;
+    if(!users.find(user => user.username === tweet.username)) return res.send("UNAUTHORIZED");
+    tweets.push(tweet);
     res.send("OK");
 });
 
